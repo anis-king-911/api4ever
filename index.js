e6389d17a3cadfc519eee30fa4f7456cdf0640ea
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2023-03-30 22:08:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 7700;
 
+app.use( express.json() );
+
 const { data } = require('./manga4ever.js');
 
 app.get('/', async (req, res) => {
